fda367baeb8f02b177a0aa9a7b108aca5a20ccb3
linhda <doanhlinh09@gmail.com>
2024-01-16 14:28:22 +0700

    handle error resolvers

--- src/models/admin/admin.resolver.ts
+++ src/models/admin/admin.resolver.ts
@@ -4,9 +4,10 @@ import { Admin } from './admin.model';
 import { UpdateAdminInput } from './dto/update-admin.input';
 import { SetRole } from '../../auth/decorators/set-role.decorator';
 import { Role } from '../../common/enums/common.enums';
-import { UseGuards } from '@nestjs/common';
+import { HttpStatus, UseGuards } from '@nestjs/common';
 import { JwtAccessGuard } from '../../auth/guards/jwt-access.guard';
 import { RolesGuard } from '../../auth/guards/role.guard';
+import { BaseException } from '../../common/filters/exception.filter';
 
 @Resolver(() => Admin)
 export class AdminResolver {
@@ -15,29 +16,55 @@ export class AdminResolver {
   @Query(() => [Admin], { name: 'ListAdmin' })
   @SetRole(Role.ADMIN)
   @UseGuards(JwtAccessGuard, RolesGuard)
-  findAll() {
-    return this.adminService.findAll();
+  async findAll() {
+    return await this.adminService.findAll();
   }
 
-  @Query(() => Admin, { name: 'GetOneAdmin' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.adminService.findOne(id);
+  @Query(() => Admin, { name: 'GetOneAdmin', nullable: true })
+  async findOne(@Args('id', { type: () => Int }) id: number) {
+    return await this.adminService.findOne(id);
   }
 
   @Mutation(() => Admin, { name: 'UpdateAdmin' })
   @SetRole(Role.ADMIN)
   @UseGuards(JwtAccessGuard, RolesGuard)
-  updateAdmin(
+  async updateAdmin(
     @Args('id') id: number,
     @Args('updateAdminInput') updateAdminInput: UpdateAdminInput
   ) {
-    return this.adminService.update(id, updateAdminInput);
+    try {
+      const adminExist = await this.adminService.findOne(id);
+
+      if (!adminExist) {
+        throw new BaseException({
+          message: 'Admin does not exist',
+          statusCode: HttpStatus.NOT_MODIFIED
+        })
+      }
+      return await this.adminService.update(id, updateAdminInput);
+    }
+    catch (e) {
+      return e;
+    }
   }
 
   @Mutation(() => Admin, { name: 'DeleteAdmin' })
   @SetRole(Role.ADMIN)
   @UseGuards(JwtAccessGuard, RolesGuard)
-  removeAdmin(@Args('id', { type: () => Int }) id: number) {
-    return this.adminService.remove(id);
+  async removeAdmin(@Args('id', { type: () => Int }) id: number) {
+    try {
+      const adminExist = await this.adminService.findOne(id);
+
+      if (!adminExist) {
+        throw new BaseException({
+          message: 'Admin does not exist',
+          statusCode: HttpStatus.NOT_MODIFIED
+        })
+      }
+      return await this.adminService.remove(id);
+    }
+    catch (e) {
+      return e;
+    }
   }
 }
--- src/models/user/user-admin.resolver.ts
+++ src/models/user/user-admin.resolver.ts
@@ -4,10 +4,11 @@ import { User } from './user.model';
 import { UpdateUserInput } from './dto/update-user.input';
 import { SetRole } from '../../auth/decorators/set-role.decorator';
 import { Role } from '../../common/enums/common.enums';
-import { ParseEnumPipe, UseGuards } from '@nestjs/common';
+import { HttpStatus, ParseEnumPipe, UseGuards } from '@nestjs/common';
 import { JwtAccessGuard } from '../../auth/guards/jwt-access.guard';
 import { RolesGuard } from '../../auth/guards/role.guard';
 import { Status } from '@prisma/client';
+import { BaseException } from '../../common/filters/exception.filter';
 
 @Resolver(() => User)
 export class UserAdminResolver {
@@ -22,7 +23,7 @@ export class UserAdminResolver {
     return await this.userService.findAll();
   }
 
-  @Query(() => User, { name: 'GetOneUser' })
+  @Query(() => User, { name: 'GetOneUser', nullable: true })
   @SetRole(Role.ADMIN)
   @UseGuards(JwtAccessGuard, RolesGuard)
   async findOne(@Args('id', { type: () => Int }) id: number) {
@@ -34,15 +35,43 @@ export class UserAdminResolver {
   @UseGuards(JwtAccessGuard, RolesGuard)
   async updateUser(
     @Args('id') id: number,
-    @Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return await this.userService.update(id, updateUserInput);
+    @Args('updateUserInput') updateUserInput: UpdateUserInput
+  ) {
+    try {
+      const userExist = await this.userService.findOne(id);
+
+      if (!userExist) {
+        throw new BaseException({
+          message: 'User does not exist',
+          statusCode: HttpStatus.NOT_MODIFIED
+        })
+      }
+
+      return await this.userService.update(id, updateUserInput);
+    }
+    catch (e) {
+      return e;
+    }
   }
 
   @Mutation(() => User, { name: 'DeleteUser' })
   @SetRole(Role.ADMIN)
   @UseGuards(JwtAccessGuard, RolesGuard)
   async deleteUser(@Args('id', { type: () => Int }) id: number) {
-    return await this.userService.remove(id);
+    try {
+      const userExist = await this.userService.findOne(id);
+
+      if (!userExist) {
+        throw new BaseException({
+          message: 'User does not exist',
+          statusCode: HttpStatus.NOT_MODIFIED
+        })
+      }
+      return await this.userService.remove(id);
+    }
+    catch (e) {
+      return e;
+    }
   }
 
   @Mutation(() => User, { name: 'UpdateUserStatus' })
@@ -50,7 +79,21 @@ export class UserAdminResolver {
   @UseGuards(JwtAccessGuard, RolesGuard)
   async updateUserStatus(
     @Args('id') id: number,
-    @Args('status', new ParseEnumPipe(Status)) status: Status) {
-    return await this.userService.updateStatus(id, status);
+    @Args('status', new ParseEnumPipe(Status)) status: Status
+  ) {
+    try {
+      const userExist = await this.userService.findOne(id);
+
+      if (!userExist) {
+        throw new BaseException({
+          message: 'User does not exist',
+          statusCode: HttpStatus.NOT_MODIFIED
+        })
+      }
+      return await this.userService.updateStatus(id, status);
+    }
+    catch (e) {
+      return e;
+    }
   }
 }
